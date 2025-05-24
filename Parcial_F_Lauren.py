7fc9428b4593c5769df6fd47e0332fccd0e76884
LauMarinAlvis <lvmarin1710@gmail.com>
2025-05-24 02:27:52 -0500

    avanzando parte de ventanas diccionario y algunas funciones

--- Parcial_F_Lauren.py
+++ Parcial_F_Lauren.py
@@ -16,17 +16,41 @@ import matplotlib
 #  Puntaje Final = (∑ (puntaje × dificultad)) / (∑ dificultades)
 
 
+participantes = {} #dicicionario 
 
 def regis_participante():
     nombre = simpledialog.askstring("Registre", "nombre del participante:")
     if not nombre:
         return
 
+clasifica = "Sí" if puntaje_final >= 70 else "No"        
+
+puntajes 
+dificultad
+
+participantes[nombre] = {
+            "puntajes": puntajes,
+            "dificultades": dificultades,
+            "puntaje_final": puntaje_final,
+            "clasifica": clasifica
+        }
 
 def report_general():
+    if not participantes: 
+        messagebox.showinfo("Reporte", "No hay participantes registrados.") 
+        return
+
 def reporte_individual():
 
 
 
 ventana = tk.Tk()
 ventana.title("Sistema de Rendimiento Pruebas Deportivas")
+
+tk.Label(ventana, text="Menu Principal")
+tk.Button(ventana, text="1. Registrar Participante")
+tk.Button(ventana, text="2. Reporte General")
+tk.Button(ventana, text="3. Reporte Individual")
+tk.Button(ventana, text="4. salir")
+
+ventana.mainloop()
