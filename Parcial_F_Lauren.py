762722e5d360776b42bd7a03cda5db1bc6591620
LauMarinAlvis <lvmarin1710@gmail.com>
2025-05-24 03:15:04 -0500

    finalice regis participantes

--- Parcial_F_Lauren.py
+++ Parcial_F_Lauren.py
@@ -19,38 +19,67 @@ import matplotlib
 participantes = {} #dicicionario 
 
 def regis_participante():
-    nombre = simpledialog.askstring("Registre", "nombre del participante:")
+    nombre = simpledialog.askstring("Registro", "nombre del participante:")
     if not nombre:
-        return
+        return 
 
-clasifica = "Sí" if puntaje_final >= 70 else "No"        
+    try:
+        puntajes = []
+        dificultades = []
+        pruebas = ["resistencia", "fuerza", "velocidad"]
+        for prueba in pruebas:
+            puntaje = float(simpledialog.askstring("Registro", f"ingrese puntaje {prueba} (0-100):"))
+            if puntaje < 0 or puntaje > 100:
+                raise ValueError("fuera de rango")
+            dificultad = round(random.uniform(1.0, 1.3), 2) 
+            puntajes.append(puntaje)
+            dificultades.append(dificultad)
 
-puntajes 
-dificultad
 
-participantes[nombre] = {
+
+        
+        suma_ponderada = sum([p * d for p, d in zip(puntajes, dificultades)])
+        suma_dificultades = sum(dificultades)
+        puntaje_final = round(suma_ponderada / suma_dificultades)
+
+        clasifica = "si" if puntaje_final >= 70 else "no"
+
+        participantes[nombre] = {
             "puntajes": puntajes,
             "dificultades": dificultades,
             "puntaje_final": puntaje_final,
             "clasifica": clasifica
         }
 
+        messagebox.showinfo("regis participante", f"participante {nombre} ingreso OK.\nPuntaje final: {puntaje_final}\nClasifica: {clasifica}")
+
+    except ValueError as e:
+        messagebox.showerror("atencion", f"invalido: {str(e)}. Solo numeros entre 0 y 100.")
+
 def report_general():
     if not participantes: 
         messagebox.showinfo("Reporte", "No hay participantes registrados.") 
         return
 
+
 def reporte_individual():
+    nombre = simpledialog.askstring("Buscar participante", "nombre del participante:")
+    if not nombre: 
+        return
+
+df = pd.DataFrame.from_dict (participantes, orient="index")   
+
 
 
 
 ventana = tk.Tk()
 ventana.title("Sistema de Rendimiento Pruebas Deportivas")
 
-tk.Label(ventana, text="Menu Principal")
-tk.Button(ventana, text="1. Registrar Participante")
-tk.Button(ventana, text="2. Reporte General")
-tk.Button(ventana, text="3. Reporte Individual")
-tk.Button(ventana, text="4. salir")
+tk.Label(ventana, text="Menu Principal", font=("Arial", 18)).pack(pady=10) 
+tk.Button(ventana, text="1. Registrar Participante", width=45, command=regis_participante).pack(pady=10)
+tk.Button(ventana, text="2. Reporte General", width=45, command=report_general).pack(pady=10) 
+tk.Button(ventana, text="3. Reporte Individual",  width=45, command=reporte_individual).pack(pady=10)
+tk.Button(ventana, text="4. salir", width=45, command=ventana.quit).pack(pady=15) 
+
 
 ventana.mainloop()
